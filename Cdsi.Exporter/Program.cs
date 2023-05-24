1b7412613bc0b8722c8505128a883aeaceb95218
Dennis Dunn <ansofive@gmail.com>
2023-05-24 17:59:23 -0400

    Added a setup() method to ensure the output folder exists.

--- Cdsi.Exporter/Program.cs
+++ Cdsi.Exporter/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Text.Json;
 
@@ -6,14 +7,23 @@ namespace OpenCdsi.Export
 {
     class Program
     {
+        private const string DefaultBuildFolder = @"..\..\..\..\results\";
+
         private static JsonSerializerOptions _options = new JsonSerializerOptions
         {
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             // etc.
         };
 
+        static void Setup()
+        {
+            Directory.CreateDirectory(DefaultBuildFolder);
+        }
+
         static void Main(string[] args)
         {
+            Setup();
+
             Export("caselibrary", CaseLibrary.Cases.Values);
             Export("antigens", Cdsi.Antigens.Values.Select(Antigen.CreateFrom));
             Export("vaccines", Cdsi.Schedule.Vaccines);
@@ -25,7 +35,7 @@ namespace OpenCdsi.Export
         static void Export(string basename, object obj)
         {
             var data = JsonSerializer.Serialize(obj, _options);
-            File.WriteAllText(@"..\..\..\..\results\" + basename + ".json", data);
+            File.WriteAllText(DefaultBuildFolder + basename + ".json", data);
         }
     }
 }
